ad6a0976b5ad5c8ee73cbe6b040d1359822a0215
mari rivas lopez <mari2006rivaslopez@outlook.com>
2026-04-09 06:05:42 -0600

    Se subio el model de categoria

--- SistemadeVentas.IUMVC/Models/CategoriaModels.cs
+++ SistemadeVentas.IUMVC/Models/CategoriaModels.cs
@@ -1,6 +1,29 @@
-﻿namespace SistemadeVentas.IUMVC.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemadeVentas.IUMVC.Models
 {
-    public class CategoriaModels
+    public class Categoria
     {
+        
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estatus es obligatorio.")]
+        [Display(Name = "Estatus")]
+        public bool Estatus { get; set; }
+
+        
+        // Un Categoria puede tener muchos Productos
+        public virtual ICollection<Producto> Productos { get; set; }
     }
 }
