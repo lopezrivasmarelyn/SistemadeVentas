9d6e3c2fe64e75ab90203d8dd397febfacfbdaf4
Gerardo <gerardleon07@icloud.com>
2026-04-09 23:59:09 -0600

    creacion de index/home y creacion de vista Rol con sus metodos crear, modificar y eliminar y su index

---
+++ SistemadeVentas.IUMVC/Views/Rol/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemadeVentas.IUMVC.Views.Rol
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
